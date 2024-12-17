b8b11fc975c2065f983d7629e6a3090229a97520
sara viana <saraviana04@gmail.com  git config --global user.name sara>
2024-12-17 19:07:26 -0300

    Primeiros dados do projeto

---
+++ Gerenciamento_de_Reservas_Salas/src/test/java/com/example/Gerenciamento_de_Reservas_Salas/GerenciamentoDeReservasSalasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gerenciamento_de_Reservas_Salas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeReservasSalasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
